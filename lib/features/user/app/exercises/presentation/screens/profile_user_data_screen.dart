af326939fc289d1d161fa1749a6842f9c2727a57
ahmedfayez90 <122109723+ahmedfayez90@users.noreply.github.com>
2023-02-24 15:21:56 +0200

    edit user data screen

--- lib/features/user/app/exercises/presentation/screens/profile_user_data_screen.dart
+++ lib/features/user/app/exercises/presentation/screens/profile_user_data_screen.dart
@@ -1,4 +1,3 @@
-import 'package:top_gym/features/user/app/exercises/presentation/date/controllers/user_details_controllers.dart';
 import 'package:top_gym/features/user/app/exercises/presentation/view/user_details/field_user_details.dart';
 import '../../../../../../core/config/routes/app.dart';
 
@@ -23,49 +22,17 @@ class ProfileUSerDataScreen extends StatelessWidget {
           "Edit Profile",
           style: TextStyle(color: Theme.of(context).cardColor),
         ),
-        actions: [
-          BlocBuilder<ProfileUserCubit, ProfileUserState>(
-            builder: (context, state) {
-              return IconButton(
-                splashColor: AppColor.noColor,
-                onPressed: () {
-                  // context.read<ProfileUserCubit>().updateUser(
-                  //       name: UserDetailsControllers.nameController.text,
-                  //       phone: UserDetailsControllers.phoneController.text,
-                  //       email: UserDetailsControllers.emailController.text,
-                  //       age: UserDetailsControllers.ageController.text,
-                  //       country: UserDetailsControllers.countryController.text,
-                  //       zipCode: UserDetailsControllers.zipCodeController.text,
-                  //       height: UserDetailsControllers.heightController.text,
-                  //       weight: UserDetailsControllers.weightController.text,
-                  //       male: UserDetailsControllers.genderController.text ==
-                  //               'male'
-                  //           ? true
-                  //           : false,
-                  //       feMale: UserDetailsControllers.genderController.text ==
-                  //               'male'
-                  //           ? false
-                  //           : true,
-                  //       isUser: UserDetailsControllers.userController.text,
-                  //       phoneNumber: UserDetailsControllers.nameController.text,
-                  //     );
-                },
-                icon: Icon(
-                  Icons.check,
-                  size: 25.sp,
-                  color: Theme.of(context).cardColor,
-                ),
-              );
-            },
-          ),
-        ],
+      
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 3.w),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const ProfilePhoto(),
+            const ProfilePhoto(
+              isButton: true,
+              radius: 80,
+            ),
             SizedBox(height: 3.h),
             Text(
               'Your Information',
@@ -75,25 +42,7 @@ class ProfileUSerDataScreen extends StatelessWidget {
                   ),
             ),
             SizedBox(height: 3.h),
-            // BlocConsumer<ProfileUserCubit, ProfileUserState>(
-            //   listener: (context, state) {
-            // if (state is UpdateUserDetailsSuccessState) {
-            //   flutterToast(
-            //       msg: "Success Edit", color: AppColor.primaryColor);
-            // } else if (state is UpdateUserDetailsErrorState) {
-            //   flutterToast(msg: "Error", color: AppColor.primaryColor);
-            // } else if (state is UpdateUserDetailsLoadingState) {
-            //   const Center(
-            //     child: CircularProgressIndicator(),
-            //   );
-            // }
-            // },
-            // builder: (context, state) {
-            //   var cubit = ProfileUserCubit.get(context);
-            //   return
             FieldUserDetails(),
-            //   },
-            // ),
           ],
         ),
       ),
