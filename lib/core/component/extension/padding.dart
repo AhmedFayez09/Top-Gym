c3d86cae15bf0c2eb38e2589ccfc588a6c0f42bf
ahmedfayez90 <122109723+ahmedfayez90@users.noreply.github.com>
2023-03-01 17:39:31 +0200

    add extension

---
+++ lib/core/component/extension/padding.dart
@@ -0,0 +1,6 @@
+import 'package:top_gym/core/config/routes/app.dart';
+
+extension EmptyPadding on num {
+  SizedBox get ph => SizedBox(height: toDouble().h);
+  SizedBox get pw => SizedBox(width: toDouble().w);
+}
